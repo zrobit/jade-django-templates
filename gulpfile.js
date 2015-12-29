50e4f0a4dfaf0c9b89d5ab88e3d23eb2e2d556de
zrobit <luismigre47@gmail.com>
2015-12-28 21:17:01 -0500

    update else statament

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ gulp.task('templates-django', function () {
     //replace vars
     .pipe(replace(/(for [a-z]+ in [a-z\.]+[a-z])$/gm,'  |{% $1 %}'))
     .pipe(replace(/else if ([a-z \.\=]+[a-z0-9])$/gm,'  |{% elif $1 %}'))
+    .pipe(replace(/else$/gm,'  |{% else %}'))
     .pipe(replace(/(if [a-z \.\=]+[a-z])$/gm,'  |{% $1 %}'))
     .pipe(replace(/\/\/- ?end(for|if)$/gm,'|{% end$1 %}'))
     .pipe(replace(/( {2,})\|(?:{% ?if [a-z\. \=]+ ?%})\n([^\t]*)\n\1\|(?:{% ?endif ?%})/g, function(s){
